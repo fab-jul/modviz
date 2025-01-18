bfaccb2431ca428e493bca26769ae7bf069ac902
Fabian <mentzerf@gmail.com>
2025-01-18 10:50:06 +0100

    Initial Commit

---
+++ modviz/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  modviz
+//
+//  Created by Fabian Mentzer on 18.01.2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ modvizTests/modvizTests.swift
@@ -0,0 +1,17 @@
+//
+//  modvizTests.swift
+//  modvizTests
+//
+//  Created by Fabian Mentzer on 18.01.2025.
+//
+
+import Testing
+@testable import modviz
+
+struct modvizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
